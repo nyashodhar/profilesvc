c3c21aa51299359bf5ea99263a3a556419f47b53
Per Blix <per.blix@mac.com>
2014-09-04 22:56:02 -0700

    Remove active record to enable mongo storage layer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ include AuthServiceMockHelper
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  #fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
